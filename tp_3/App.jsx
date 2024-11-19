707ce5dfd7d1fe33350a91fc00fd197bbba21280
Remy Pierre <remy.pierre.dev@outlook.fr>
2024-11-19 09:46:15 +0100

    tp 3

---
+++ tp_3/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="green">
+          <img src="../public/24.webp" width="80" />
+        </div>
+        <div></div>
+        <div>
+          <img src="../public/3.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/4.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/5.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+        <div>
+          <img src="../public/1.webp" width="80" />
+        </div>
+      </div>
+      <div id="demo">
+        <button id="btnTricher">Tricher</button>
+      </div>
+    </>
+  );
+}
